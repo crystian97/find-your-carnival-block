934c547fe2859bcc8909a9e1dc9c546fb5bbfd81
crystian97 <icrystianveloso@gmail.com>
2023-02-19 22:42:33 -0300

    feat:config theme to project

---
+++ src/styles/themes/default.js
@@ -0,0 +1,10 @@
+const defaultTheme = {
+  primary: "#6246EA",
+  icons: "#E45858",
+  text: "#121214",
+  white: "#FFFFFF",
+  "gray-100": "#EAEAEA",
+  "gray-200": "#F4F4FF",
+  "gray-300": "#858793",
+};
+export default defaultTheme;
