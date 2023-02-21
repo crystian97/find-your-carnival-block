f9ba831be40b937b28525bc05710693adda19822
crystian97 <icrystianveloso@gmail.com>
2023-02-20 22:28:56 -0300

    feat:setup global styles css

---
+++ src/styles/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+body{
+    width: 100%;
+    background-color: #F8F8FF;
+}
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
+}
+
+
+`;
