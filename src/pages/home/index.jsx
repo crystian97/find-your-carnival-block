7b34e39b6c4fc30bb81e5de5104d30b4cde6ab8b
crystian97 <icrystianveloso@gmail.com>
2023-02-22 17:11:00 -0300

    feat:call api to show all blocks

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { api } from "../../libs/api/axios";
 import Content from "./components/Content";
 import Hero from "./components/Hero";
 import { HomeContainer } from "./styles";
 
 const Home = () => {
+  async function getBlocks() {
+    await api.get("/carnival-blocks").then((response) => {
+      const b = response.data;
+      setBlocks(b);
+    });
+  }
+  const [blocks, setBlocks] = useState([]);
+  useEffect(() => {
+    getBlocks();
+  }, []);
+  console.log(blocks);
   return (
     <HomeContainer>
       <Hero />
-      <Content />
+      <Content blocks={blocks} />
     </HomeContainer>
   );
 };
